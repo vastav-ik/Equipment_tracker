0a26883cb8d9d5b58045c5e21601177a4b9a7770
vastav-ik <guptavastav9389@gmail.com>
2025-12-17 20:31:22 +0530

    Create the database with the checks and add the environment variables and the pool

---
+++ server/db.js
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+import { configDotenv } from "dotenv";
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+export default pool;
